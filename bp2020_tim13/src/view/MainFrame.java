0906707629dd8e58a99fea3feda95bd5ca506548
klini <klini@DESKTOP-CQHEEHC>
2020-05-23 23:45:27 +0200

    mainframe

--- bp2020_tim13/src/view/MainFrame.java
+++ bp2020_tim13/src/view/MainFrame.java
@@ -1,18 +1,80 @@
  package view;
 
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import controller.ActionManager;
+import model.Database;
+import model.DatabaseTreeModel;
+import model.Table;
+import view.treeDatabase.DatabaseTree;
 
 
 public class MainFrame extends JFrame{
 
+	private JPanel upperView;
+	private JPanel lowerView;
+	private TableView tv;
+	private DatabaseView dv;
+	private DatabaseTree dt;
+	private DatabaseTreeModel dtm;
 	private ActionManager am;
+	
 	private static MainFrame instance = null;
 	
-	private MainFrame() {
-		am = new ActionManager();
+	private MainFrame() {}
 		
+	private void initializeTree(Database d) {
+		dt = new DatabaseTree();
+		dtm = new DatabaseTreeModel(d);
+		dt.setModel(dtm);
+		dt.addMouseListener(new MouseAdapter());
+		
+	}
+
+	private void initializeGUI() {
+		
+		
+	}
+	
+	private void initialize(Database d) {
+		am = new ActionManager();
+		initializeTree(d);
+		initializeGUI();
+	}
+	
+	public JPanel getUpperView() {
+		return upperView;
+	}
+
+	public void setUpperView(JPanel upperView) {
+		this.upperView = upperView;
+	}
+
+	public JPanel getLowerView() {
+		return lowerView;
+	}
+
+	public void setLowerView(JPanel lowerView) {
+		this.lowerView = lowerView;
+	}
+
+	public ActionManager getAm() {
+		return am;
+	}
+	
+	class MouseAdapter extends java.awt.event.MouseAdapter{
+		public void mouseClicked(MouseEvent ms) {
+			if(ms.getClickCount()==2) {
+				Object o = dt.getLastSelectedPathComponent();
+				if(o instanceof Table) {
+					
+				}
+			}
+		}
 	}
 	
 	public static MainFrame getInstance() {
@@ -20,8 +82,5 @@ public class MainFrame extends JFrame{
 			instance = new MainFrame();
 		return instance;
 	}
-
-	public ActionManager getAm() {
-		return am;
-	}
+	
 }
