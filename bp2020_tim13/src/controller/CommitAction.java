452acccd0d3e15853266fd0814cd784b9eefcddf
klini <klini@DESKTOP-CQHEEHC>
2020-05-24 15:17:31 +0200

    jyuhrf

---
+++ bp2020_tim13/src/controller/CommitAction.java
@@ -0,0 +1,13 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+
+public class CommitAction extends ActionAbstract {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
