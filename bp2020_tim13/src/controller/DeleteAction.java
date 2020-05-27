cc23f973396a27f69e1b5243d2f01e9f5ceedb06
lukazecevic <lukazecevic@localhost>
2020-05-27 17:53:45 +0200

    delete uradjen

--- bp2020_tim13/src/controller/DeleteAction.java
+++ bp2020_tim13/src/controller/DeleteAction.java
@@ -2,9 +2,17 @@ package controller;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.swing.JScrollPane;
 
+import app.AppCore;
+import model.Column;
+import model.ColumnType;
+import model.Row;
 import model.Table;
 import view.MainFrame;
 import view.MyTableModel;
@@ -25,11 +33,33 @@ public class DeleteAction extends ActionAbstract {
 			MyTableModel mdl = (MyTableModel) t.getModel();
 			
 			Table table = t.getTable();
+			Row r = table.getRows().get(t.getSelectedRow());
+			
+			Connection connection = AppCore.startConnection();
+			try {
+				String prmk = null;
+			DatabaseMetaData dmd = connection.getMetaData();
+			ResultSet primKey = dmd.getPrimaryKeys(connection.getCatalog(), null, table.getName());
+			while(primKey.next()) {
+				 prmk = primKey.getString("COLUMN_NAME");
+			}
+			String value = (String) r.getFields().get(prmk);
+			Column c = table.getChildNode(prmk);
+			String query = null;
+			if(c.getType().equals(ColumnType.INT))
+			 query = "DELETE FROM " + table.getName() +   " WHERE " + prmk + " = "+Integer.parseInt(value);
+			else if(c.getType().equals(ColumnType.FLOAT))
+				 query = "DELETE FROM " + table.getName() +   " WHERE " + prmk + " = "+Float.parseFloat(value);
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.executeUpdate();
+			}
+			catch(Exception es) {
+				es.printStackTrace();
+			}
+			
 			table.removeRows(table.getRows().get(t.getSelectedRow()));
-
-			//int index = t.getSelectedRow();
-			//mdl.removeRow(index);
-			//t.getTable().removeRows(t.getRows().get(index)); 
+			
+			AppCore.CloseConnection(connection);
 		}
 
 	}
