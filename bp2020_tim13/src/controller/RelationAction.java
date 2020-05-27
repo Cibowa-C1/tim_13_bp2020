8beb2de46826c0cc95249780b05262f4199b5b81
lukazecevic <lukazecevic@localhost>
2020-05-28 00:46:46 +0200

    relation pocetak

--- bp2020_tim13/src/controller/RelationAction.java
+++ bp2020_tim13/src/controller/RelationAction.java
@@ -2,10 +2,17 @@ package controller;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JScrollPane;
 
-
+import app.AppCore;
+import model.Row;
+import model.Table;
 import view.MainFrame;
 import view.TableView;
 
@@ -15,14 +22,42 @@ public class RelationAction extends ActionAbstract{
 		putValue(NAME, "Relation");
 		putValue(SHORT_DESCRIPTION, "Relation");
 	}
+	private int getColumnByName(TableView table, String name) {
+	    for (int i = 0; i < table.getColumnCount(); ++i)
+	        if (table.getColumnName(i).equals(name))
+	            return i;
+	    return -1;
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		List<Table> tables = new ArrayList<Table>();
 		Component o = MainFrame.getInstance().getdV().getJtp().getSelectedComponent();
 		if(o instanceof JScrollPane) {
 			JScrollPane scrl =(JScrollPane)o;
 			TableView t = (TableView) scrl.getViewport().getComponents()[0];
-			int selRow = t.getSelectedRow();
-		
+			Table table = t.getTable();
+			Row r = table.getRows().get(t.getSelectedRow());
+			int rowIndex = t.getSelectedRow();
+			
+			Connection connection = AppCore.startConnection();
+			try {
+				String prmk = null;
+				DatabaseMetaData dmd = connection.getMetaData();
+				ResultSet primKey = dmd.getPrimaryKeys(connection.getCatalog(), null, table.getName());
+				while(primKey.next()) {
+					prmk = primKey.getString("COLUMN_NAME");
+			}
+				String value = (String) t.getValueAt(rowIndex, getColumnByName(t, prmk));
+				for (Table tblMod : table.getDatabase().getChildren()) {
+					if(tblMod.getName().equals(table.getName())) continue;
+					if(tblMod.getChildNode(prmk)!=null) {
+						tables.add(tblMod);
+					}
+				}
+				
+			}catch(Exception es) {
+				
+			}
 	}
 		
 	}
