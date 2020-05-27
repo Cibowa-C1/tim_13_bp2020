c2a2ce104f1efc0b542b24fbd2f2d4560a910abd
lukazecevic <lukazecevic@localhost>
2020-05-27 22:25:56 +0200

    update uradjen

--- bp2020_tim13/src/controller/UpdateAction.java
+++ bp2020_tim13/src/controller/UpdateAction.java
@@ -1,6 +1,26 @@
 package controller;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.function.BiFunction;
+
+import javax.swing.JScrollPane;
+
+import app.AppCore;
+import model.Column;
+import model.ColumnType;
+import model.Row;
+import model.Table;
+import view.MainFrame;
+import view.MyTableModel;
+import view.OptionDialog;
+import view.TableView;
 
 public class UpdateAction extends ActionAbstract{
 
@@ -8,11 +28,95 @@ public class UpdateAction extends ActionAbstract{
 		putValue(NAME, "Update");
 		putValue(SHORT_DESCRIPTION, "Update");
 	}
+	 
+	
+	private int getColumnByName(TableView table, String name) {
+	    for (int i = 0; i < table.getColumnCount(); ++i)
+	        if (table.getColumnName(i).equals(name))
+	            return i;
+	    return -1;
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		Connection connection=null;
+		HashMap<String, Object> mapaKopi = new HashMap<String, Object>();
 		
-		//
-		
+		Component o = MainFrame.getInstance().getdV().getJtp().getSelectedComponent();
+		if(o instanceof JScrollPane) {
+			JScrollPane scrl =(JScrollPane)o;
+			TableView t = (TableView) scrl.getViewport().getComponents()[0];
+			MyTableModel mdl = (MyTableModel) t.getModel();
+			
+			
+			
+			Table table = t.getTable();
+			Row r = table.getRows().get(t.getSelectedRow());
+			mapaKopi.putAll(r.getFields());
+			int rowIndex = t.getSelectedRow();
+			
+			 connection = AppCore.startConnection();
+			try {
+				DatabaseMetaData dmd = connection.getMetaData();
+				ResultSet primKey = dmd.getPrimaryKeys(connection.getCatalog(), null, table.getName());
+				String prmk = null;
+				StringBuilder sb = new StringBuilder("UPDATE " + table.getName() + " SET ");
+				while(primKey.next()) {
+					 prmk = primKey.getString("COLUMN_NAME");
+				}
+				int flag = 1;
+				for (Column c : table.getChildren()) {
+					if(c.getName().equals(prmk)) continue;
+					int colIndex = getColumnByName(t, c.getName());
+					String value = (String) t.getValueAt(rowIndex, colIndex);
+					if(flag==1) {
+					sb.append(c.getName()+" = ");
+					flag = 0;
+					}else
+						sb.append(", " + c.getName() + " = ");
+					r.replaceValue(c.getName(), value);
+					if(c.getType().equals(ColumnType.NUMERIC) || (c.getType().equals(ColumnType.INT)))
+						sb.append(Integer.parseInt(value));
+					else if(c.getType().equals(ColumnType.FLOAT)) {
+						sb.append(Float.parseFloat(value));
+					}	
+					else if((c.getType().equals(ColumnType.CHAR)||(c.getType().equals(ColumnType.VARCHAR)))){
+						sb.append( "'"+value+"'");
+
+					}
+					else if(c.getType().equals(ColumnType.DATETIME)){
+						
+						sb.append( "'"+value+"'");
+					}
+					else
+						sb.append(value);
+				}
+				int colIndex = getColumnByName(t, prmk);
+				String value = (String) t.getValueAt(rowIndex, colIndex);
+				Column coll = table.getChildNode(prmk);
+				if(coll.getType().equals(ColumnType.NUMERIC) || (coll.getType().equals(ColumnType.INT)))
+					sb.append(" WHERE " + prmk + " = "+Integer.parseInt(value));
+				else if(coll.getType().equals(ColumnType.FLOAT)) {
+					sb.append(" WHERE " + prmk + " = "+Float.parseFloat(value));
+				}	
+				else if((coll.getType().equals(ColumnType.CHAR)||(coll.getType().equals(ColumnType.VARCHAR)))){
+					sb.append( " WHERE " + prmk + " = "+"'"+value+"'");
+
+				}
+				else if(coll.getType().equals(ColumnType.DATETIME)){
+					
+					sb.append( " WHERE " + prmk + " = "+"'"+value+"'");
+				}
+				else
+					sb.append(value);
+				PreparedStatement ps = connection.prepareStatement(sb.toString());
+				ps.executeUpdate();
+				System.out.println(sb.toString());
+			} catch (SQLException e1) {
+				r.getFields().putAll(mapaKopi);
+				OptionDialog op = new OptionDialog("Unesi validne podatke");
+			}
+		}
+		AppCore.CloseConnection(connection);
 	}
 
 }
--- bp2020_tim13/src/model/Row.java
+++ bp2020_tim13/src/model/Row.java
@@ -1,19 +1,27 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import observer.IListener;
+import observer.IObserver;
+import observer.state.ObserverStates;
 
 
-public class Row {
+
+public class Row implements IObserver{
 	
 	 private String name;
 	    private Map<String, Object> fields;
+	    private List<IListener> listeners;
 	    
 	    
 	    public Row(String name) {
 	        this.fields = new HashMap<>();
 	        this.name = name;
+	        listeners = new ArrayList<IListener>();
 	    }
 
 	    public void addField(String fieldName, Object value) {
@@ -38,6 +46,37 @@ public class Row {
 
 		public void setFields(Map<String, Object> fields) {
 			this.fields = fields;
+		}
+		public void replaceValue(String key,Object value) {
+			fields.replace(key, value);
+			notifyObserver(ObserverStates.UPDATE, this);
+		}
+
+		@Override
+		public void addObserver(IListener listener) {
+			if(listener==null) return;
+			if(this.listeners==null) 
+				this.listeners = new ArrayList<>();
+			if(this.listeners.contains(listener)) return;
+			this.listeners.add(listener);
+		}
+
+		@Override
+		public void removeObserver(IListener listener) {
+			if(listener==null) return;
+			if(this.listeners==null) return;
+			if(!this.listeners.contains(listener)) return;
+			this.listeners.remove(listener);
+		}
+
+		@Override
+		public void notifyObserver(Object event, Object obj) {
+			if(event==null) return;
+			if(this.listeners==null) return;
+			if(this.listeners.isEmpty()) return;
+			for(IListener l : listeners) {
+				l.update(event,obj);
+			}
 		}
 	    
 }
