e840ea7e434040cd625b4eb77e32fb5ca2055464
lukazecevic <lukazecevic@localhost>
2020-05-27 20:22:06 +0200

    cass

--- bp2020_tim13/src/controller/UpdateAction.java
+++ bp2020_tim13/src/controller/UpdateAction.java
@@ -1,18 +1,6 @@
 package controller;
 
-import java.awt.Component;
 import java.awt.event.ActionEvent;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-
-import javax.swing.JScrollPane;
-
-import app.AppCore;
-import model.Row;
-import model.Table;
-import view.MainFrame;
-import view.MyTableModel;
-import view.TableView;
 
 public class UpdateAction extends ActionAbstract{
 
@@ -23,23 +11,8 @@ public class UpdateAction extends ActionAbstract{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		Component o = MainFrame.getInstance().getdV().getJtp().getSelectedComponent();
-		if(o instanceof JScrollPane) {
-			JScrollPane scrl =(JScrollPane)o;
-			TableView t = (TableView) scrl.getViewport().getComponents()[0];
-			MyTableModel mdl = (MyTableModel) t.getModel();
-			
-			Table table = t.getTable();
-			Row r = table.getRows().get(t.getSelectedRow());
-			
-			
-			Connection connection = AppCore.startConnection();
-			try {
-				DatabaseMetaData dmd = connection.getMetaData();
-			}catch (Exception es) {
-				// TODO: handle exception
-			}
-		}
+		//
+		
 	}
 
 }
