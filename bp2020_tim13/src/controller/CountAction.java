d135357bec1bcd218f2a902cd40df397f9899e4a
lukazecevic <lukazecevic@localhost>
2020-05-28 15:36:04 +0200

    joj

--- bp2020_tim13/src/controller/CountAction.java
+++ bp2020_tim13/src/controller/CountAction.java
@@ -2,12 +2,22 @@ package controller;
 
 import java.awt.event.ActionEvent;
 
+import view.TableView;
+
 public class CountAction extends ActionAbstract {
 
+	
+	private int getColumnByName(TableView table, String name) {
+	    for (int i = 0; i < table.getColumnCount(); ++i)
+	        if (table.getColumnName(i).equals(name))
+	            return i;
+	    return -1;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
