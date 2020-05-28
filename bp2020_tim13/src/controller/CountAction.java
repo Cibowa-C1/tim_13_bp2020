1fdccec5d8a5c82b9d86ca11d3c873a12e059e38
lukazecevic <lukazecevic@localhost>
2020-05-28 17:55:59 +0200

    ccasc

--- bp2020_tim13/src/controller/CountAction.java
+++ bp2020_tim13/src/controller/CountAction.java
@@ -1,10 +1,21 @@
 package controller;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 
+import javax.swing.JScrollPane;
+
+import model.Table;
+import view.CountDialog;
+import view.MainFrame;
 import view.TableView;
 
 public class CountAction extends ActionAbstract {
+	
+	public CountAction() {
+		putValue(NAME, "COUNT");
+		putValue(SHORT_DESCRIPTION, "COUNT");
+	}
 
 	
 	private int getColumnByName(TableView table, String name) {
@@ -13,11 +24,20 @@ public class CountAction extends ActionAbstract {
 	            return i;
 	    return -1;
 	}
+	private String getColumnByInd(TableView table, int ind) {
+	        
+	    return table.getColumnName(ind);
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//faa
 		
+		Component o = MainFrame.getInstance().getdV().getJtp().getSelectedComponent();
+		if(o instanceof JScrollPane) {
+			JScrollPane scrl =(JScrollPane)o;
+			TableView t = (TableView) scrl.getViewport().getComponents()[0];
+			CountDialog cd = new CountDialog(t);
+		}	
 	}
 
 }
---
+++ bp2020_tim13/src/view/CountDialog.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JDialog;
+
+import model.Table;
+
+public class CountDialog extends JDialog {
+
+	public CountDialog(TableView tableV) {
+		
+	}
+	
+}
